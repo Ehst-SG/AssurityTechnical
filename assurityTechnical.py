49c7d01350f1915bee9367e5f39f4ad0750762c4
Ehst-SG <sethgunn18@gmail.com>
2022-09-29 21:49:08 +1300

    Finish comments

--- assurityTechnical.py
+++ assurityTechnical.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Sep 28 15:28:09 2022
+
 @author: Seth Gunn
 """
 
 # =============================================================================
-# Write a procedure greedy_descent_with_random_restart(random_state, neighbours, cost) that takes three functions,
-# one to get a new random state and two to compute the neighbours or cost of a state
-# and then uses greedy_descent (you wrote earlier) to find a solution.
-# The first state in the search must be obtained by calling the function random_state.
-# The procedure must print each state it goes through (including the first and last one) in the order they are encountered.
-# When the search reaches a local minimum that is not global, the procedure must print RESTART and restart the search by calling random_state
+# This program tests the api given at https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false,
+# to the specifications:
+# Name = "Badges"
+# CanRelist = true
+# The Promotions element with Name = "Feature" has a Description that contains the text "Better position in category"
 # =============================================================================
-
 
 import unittest
 import requests
@@ -19,17 +19,21 @@ import requests
 class testApi(unittest.TestCase):
     
     def setUp(self):
+        # This function sets the web page that is to be tested and sends a request to it.
         page_url='https://api.tmsandbox.co.nz/v1/Categories/6328/Details.json?catalogue=false'
         response = requests.get(page_url)
         self.data = response.json()
 
     def testName(self):
+        # This test checks that the value for "Name" is correct.
         self.assertEqual(self.data['Name'], 'Badges')
 
     def testRelist(self):
+        # This test checks that the value for "CanRelist" is True.
         self.assertTrue(self.data['CanRelist'])
 
     def testFeature(self):
+        # This test checks that the value for the element "Feature"'s description matches the correct value.
         s = "Better position in category"
         for item in self.data['Promotions']:
             if item['Name'] == 'Feature':
